f646e1527a888516ca93fdc48310ba19713be579
Світлана Овчарук <svitlana.ovcharuk@gmail.com>
2016-12-24 03:34:05 +0200

    -

--- src/Console/Main.java
+++ src/Console/Main.java
@@ -5,10 +5,6 @@ import java.io.IOException;
 import java.util.Properties;
 import java.util.Scanner;
 
-import org.junit.runner.JUnitCore;
-import org.junit.runner.Result;
-import org.junit.runner.notification.Failure;
-
 public class Main {
 
 	public static void main(String[] args){
